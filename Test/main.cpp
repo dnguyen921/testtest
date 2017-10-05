1bdea2cddab3c5a4c87eb84d9c93d3b55383bbfe
Danny Nguyen <dannynguyen@dannys-mbp.kc.umkc.edu>
2017-10-05 16:07:17 -0500

    Initial Commit

---
+++ Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by Danny Nguyen on 10/5/17.
+//  Copyright © 2017 Danny Nguyen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
